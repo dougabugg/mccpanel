3cc4f8d1c13222b1483d338e5f5b1854964ef222
Doug <drezabek@protonmail.com>
2022-01-31 09:52:45 -0600

    Update config.py
    added Config class with better error handling

--- mccpanel/config.py
+++ mccpanel/config.py
@@ -3,6 +3,50 @@ from mccpanel.mcserver import McServer
 import os, json
 # this should not use globals, will fix eventually
 
+class Config:
+    def __init__(self, config_file=None):
+        self.config_file = None
+        self.secret_key = None
+        self.settings = None
+        self.userdb = None
+        self.servers = None
+        if config_file:
+            self.config_file = config_file
+            self.load_from_file()
+    def load_from_file(self, config_file=None):
+        if config_file:
+            self.config_file = config_file
+        else:
+            config_file = self.config_file
+        try:
+            fh = open(config_file)
+        except FileNotFoundError:
+            self.create_default_config(config_file)
+            fh = open(config_file)
+        self.settings = json.load(fh)
+        self.userdb = {}
+        for entry in self.settings["registered_users"]:
+            ukey = "user:" + entry["username"]
+            userdb[ukey] = entry
+        self.servers = OrderedDict()
+        for entry in self.settings["servers"]:
+            server = McServer(entry["name"], entry["jar_file"], entry["cwd"])
+            if server.name in self.servers:
+                server.name += "-2"
+            self.server[server.name] = server
+            if entry.get("autostart"):
+                server.start()
+    def create_default_config(self, config_file):
+        try:
+            fh = open(config_file, "w")
+        except Exception as e:
+            print(f"FATAL ERROR: unable to create config file named `{config_file}`.")
+            raise e
+        default_settings = {
+            "registered_users": [{"username": "root", "password": "root"}],
+            "servers": [{"name": "main", "jar_file": "server.jar", "cwd": "./servers/main", "autostart": True}]
+        }
+        json.dump(default_settings, fh)
 
 settings = None
 userdb = None
